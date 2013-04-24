f3d96f1a5b2fd7ad8ece36a6563f3a13b5c608e9
tino <t_perucchi@hotmail.com>
2013-04-24 23:07:51 +0200

    add paginator

--- flightlog/views.py
+++ flightlog/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 import flightlog.models as M
 import utilities
 from datetime import date, timedelta
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def sumFlights(flights):
@@ -25,14 +26,32 @@ def home(request):
 
 
 def flightlog(request):
-   flights = M.Flight.objects.all()
+   flights = M.Flight.objects.all().order_by('-date')
+   
+   paginator = Paginator(flights, 20)
+   page = request.GET.get('page')
+   try:
+      gamesPage = paginator.page(page)
+   except PageNotAnInteger:
+      # If page is not an integer, deliver the first page
+      gamesPage = paginator.page(1)
+   except EmptyPage:
+      # If page is out of range, deliver the first page
+      gamesPage = paginator.page(1) #paginator.num_pages)
+   finally:
+      HALF_RANGE = 5
+      RANGE_SIZE = 2*HALF_RANGE + 1
+      minPage = max(1, min(gamesPage.number - HALF_RANGE, paginator.num_pages - RANGE_SIZE + 1))
+      maxPage = min(max(gamesPage.number + HALF_RANGE, RANGE_SIZE), paginator.num_pages)
+      displayedPages = range(minPage, maxPage+1)
+
    return django.shortcuts.render(request,
                                   'flightlog/flightlog.html',
-                                  {'games_list' : 'a',
+                                  {'games_list' : gamesPage,
                                   'players_query_prefix' : 'a',
                                   'page_query_suffix' : 'a',
                                   'ranking_query' : 'a',
-                                  'displayed_pages' : 'a',
+                                  'displayed_pages' : displayedPages,
                                   'date_title' : 'a',
                                   'opponents_title' : 'a',
                                   'flights': flights})
