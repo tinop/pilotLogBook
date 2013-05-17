e1eda62432576bbf227bf51bd9ff498884b033fd
Tino Perucchi <tino.perucchi@dacuda.com>
2013-05-17 13:21:16 +0200

    add nvd3 local

--- pilotLogBook/settings.py
+++ pilotLogBook/settings.py
@@ -136,7 +136,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'django_nvd3',
+    #'django_nvd3',
     'flightlog'
 )
 
